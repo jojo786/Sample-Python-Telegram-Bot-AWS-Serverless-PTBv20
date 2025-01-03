ce94c42efc5b56db26a1b576677bd1d7e108a10c
Yusuf Mayet <jojo786@gmail.com>
2025-01-03 16:24:53 +0200

    update to ptb 21.10

--- ptb/ptb_lambda.py
+++ ptb/ptb_lambda.py
@@ -7,7 +7,7 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, Messa
 application = ApplicationBuilder().token("YOUR-TELEGRAM-TOKEN-HERE").build()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot running on AWS Serverless, please talk to me!")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
